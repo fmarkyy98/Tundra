0bfbfd597878118982739fe4532b118f70a6b30d
Fekete Márk <fmark98@gmail.com>
2020-04-30 17:04:33 +0200

    Unnecessery files removed.

--- Tundra/Tundra/PredatorColony.cpp
+++ Tundra/Tundra/PredatorColony.cpp
@@ -1,1 +1,27 @@
 #include "PredatorColony.h"
+
+void PredatorColony::Hunt(std::vector<PreyColony*> preyColonys)
+{
+	srand((int)time(0));
+	PreyColony* hunted;
+	int i = 0;
+	do
+	{
+		hunted = preyColonys[rand() % preyColonys.size()];
+		++i;
+	} while (hunted->getNumberOfIndividuals() == 0 && i < 10);
+	if (hunted->getNumberOfIndividuals() < hunted->DieingRate(this->_numberOfIndividuals))
+	{
+		hunted->setNumberOfIndividuals(0);
+		std::cout << hunted->getName() << " Kipusztult!!!" << std::endl;
+		(this->_numberOfIndividuals *= 3) /= 4;
+		if (this->_numberOfIndividuals < 2)
+		{
+			std::cout << this->_name << "Éhanhalt :'(" << std::endl;
+		}
+	}
+	else
+	{
+		hunted->Die(this->_numberOfIndividuals);
+	}
+}
--- Tundra/Tundra/PredatorColony.h
+++ Tundra/Tundra/PredatorColony.h
@@ -10,31 +10,31 @@ class PredatorColony : public Colony
 public:
 	PredatorColony(std::string name, int numberOfIndividuals) : Colony(name, numberOfIndividuals) { }
 
-	void Hunt(std::vector<PreyColony*> preyColonys)
-	{
-		srand((int)time(0));
-		PreyColony* hunted;
-		int i = 0;
-		do
-		{
-			hunted = preyColonys[rand() % preyColonys.size()];
-			++i;
-		} while (hunted->getNumberOfIndividuals() == 0 && i < 10);
-		if (hunted->getNumberOfIndividuals() < hunted->DieingRate(this->_numberOfIndividuals)) // Dieing rate returns an integer that represents the amount of dead members after an atack.
-		{
-			hunted->setNumberOfIndividuals(0);
-			std::cout << hunted->getName() << " Kipusztult!!!" << std::endl;
-			(this->_numberOfIndividuals *= 3) /= 4;
-			if (this->_numberOfIndividuals < 2)
-			{
-				std::cout << this->_name << "Éhanhalt :'(" << std::endl;
-			}
-		}
-		else
-		{
-			hunted->Die(this->_numberOfIndividuals); // Die reduces the (numberOfIndividuals) by (DyeingRate * parameter)
-		}
-	}
+	void Hunt(std::vector<PreyColony*> preyColonys);
+	//{
+	//	srand((int)time(0));
+	//	PreyColony* hunted;
+	//	int i = 0;
+	//	do
+	//	{
+	//		hunted = preyColonys[rand() % preyColonys.size()];
+	//		++i;
+	//	} while (hunted->getNumberOfIndividuals() == 0 && i < 10);
+	//	if (hunted->getNumberOfIndividuals() < hunted->DieingRate(this->_numberOfIndividuals))
+	//	{
+	//		hunted->setNumberOfIndividuals(0);
+	//		std::cout << hunted->getName() << " Kipusztult!!!" << std::endl;
+	//		(this->_numberOfIndividuals *= 3) /= 4;
+	//		if (this->_numberOfIndividuals < 2)
+	//		{
+	//			std::cout << this->_name << "Éhanhalt :'(" << std::endl;
+	//		}
+	//	}
+	//	else
+	//	{
+	//		hunted->Die(this->_numberOfIndividuals);
+	//	}
+	//}
 };
 
 class SnowyOwlColony : public PredatorColony
